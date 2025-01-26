d43ec1e52083f191c516bdda151bb15132e0d31c
Peter <obidaberechukwu@gmail.com>
2025-01-26 06:38:44 +0100

    Updated Syllabus Analysis Section

--- src/components/skill_test/SyllabusAnalysis.tsx
+++ src/components/skill_test/SyllabusAnalysis.tsx
@@ -1,39 +1,12 @@
-export default function SyllabusAnalysis() {
-  const Analysis = ({
-    text,
-    percentile,
-    color,
-  }: {
-    text: string;
-    percentile: number;
-    color: string;
-  }) => {
-    return (
-      <div className="flex w-full flex-col gap-4 ">
-        <div className="flex w-full">
-          <span className="text-text font-medium text-[0.875rem]">{text}</span>
-        </div>
+"use client";
+import { useEffect, useState } from "react";
 
-        <div className={`flex justify-between gap-4`}>
-          <div className={`flex w-full h-2 rounded-2xl bg-${color}/20 my-auto`}>
-            <div
-              style={{
-                width: `${percentile}%`,
-              }}
-              className={`flex transition-[width] h-full rounded-2xl bg-${color}`.toString()}
-            ></div>
-          </div>
-          <div>
-            <span
-              className={`flex font-semibold text-[0.875rem] text-${color}`}
-            >
-              {percentile}%
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  };
+export default function SyllabusAnalysis() {
+  const [percentiles, setPercentiles] = useState([0, 0, 0, 0]);
+
+  useEffect(() => {
+    setPercentiles([80, 60, 24, 96]);
+  }, []);
 
   return (
     <div className="flex w-full flex-col gap-6 border border-text/15 rounded-md px-6 py-8">
@@ -43,14 +16,26 @@ export default function SyllabusAnalysis() {
 
       <div className="flex flex-col w-full gap-6">
         {[
-          { text: "HTML Tools, Forms, History", percentile: 80, color: "blue" },
+          {
+            text: "HTML Tools, Forms, History",
+            percentile: percentiles[0],
+            color: "blue",
+          },
           {
             text: "Tags & References in HTML",
-            percentile: 60,
+            percentile: percentiles[1],
             color: "orange",
           },
-          { text: "Tables & References in HTML", percentile: 24, color: "red" },
-          { text: "Tags & CSS basics", percentile: 96, color: "green" },
+          {
+            text: "Tables & References in HTML",
+            percentile: percentiles[2],
+            color: "red",
+          },
+          {
+            text: "Tags & CSS basics",
+            percentile: percentiles[3],
+            color: "green",
+          },
         ].map((data) => (
           <Analysis key={data.text} {...data} />
         ))}
@@ -58,3 +43,37 @@ export default function SyllabusAnalysis() {
     </div>
   );
 }
+
+const Analysis = ({
+  text,
+  percentile,
+  color,
+}: {
+  text: string;
+  percentile: number;
+  color: string;
+}) => {
+  return (
+    <div className="flex w-full flex-col gap-4 ">
+      <div className="flex w-full">
+        <span className="text-text font-medium text-[0.875rem]">{text}</span>
+      </div>
+
+      <div className={`flex justify-between gap-4`}>
+        <div className={`flex w-full h-2 rounded-2xl bg-${color}/20 my-auto`}>
+          <div
+            style={{
+              width: `${percentile}%`,
+            }}
+            className={`flex transition-[width] delay-150 duration-[1.5s] ease-in-out h-full rounded-2xl bg-${color}`.toString()}
+          ></div>
+        </div>
+        <div>
+          <span className={`flex font-semibold text-[0.875rem] text-${color}`}>
+            {percentile}%
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
